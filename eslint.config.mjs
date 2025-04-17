4930ac54cea9c98263930e9524472d1dbddd0afd
vbotelhog <vbotelhog@minsait.com>
2025-04-17 12:38:37 -0300

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,34 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended"
+  ),
+  {
+    rules: {
+      "prettier/prettier": "warn",
+      "react/react-in-jsx-scope": "off", 
+      "import/order": [
+        "warn",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+ export function Navbar() {
+  return (
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a href="/">Brand</a>
+        </div>
+      </nav>
+    </header> 
+  );
+}
